e1fe13f097b5c510c19b99ee99a52672763a04ec
henok <henokkahsay43@gmail.com>
2019-10-16 14:15:08 -0700

    Updated

---
+++ PPMToolFullStack/src/main/java/com/henok/ppmtool/services/ProjectService.java
@@ -0,0 +1,19 @@
+package com.henok.ppmtool.services;
+
+import com.henok.ppmtool.domain.Project;
+import com.henok.ppmtool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project){
+
+
+        return projectRepository.save(project);
+
+    }
+}
