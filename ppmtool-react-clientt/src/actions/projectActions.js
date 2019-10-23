844f6a0638dac17b278fa63c0fdd68be16abe6a7
henok <henokkahsay43@gmail.com>
2019-10-23 13:43:38 -0700

    Get Project Final step

--- ppmtool-react-clientt/src/actions/projectActions.js
+++ ppmtool-react-clientt/src/actions/projectActions.js
@@ -3,7 +3,7 @@ import { GET_ERRORS, GET_PROJECTS } from "./types";
 
 export const createProject = (project, history) => async dispatch => {
   try {
-    const res = await axios.post("http://localhost:8080api/project", project);
+    const res = await axios.post("http://localhost:8080/api/project", project);
     history.push("/dashboard");
   } catch (err) {
     dispatch({
@@ -14,7 +14,7 @@ export const createProject = (project, history) => async dispatch => {
 };
 
 export const getProjects = () => async dispatch => {
-  const res = await axios.get("http://localhost:8080api/project/all");
+  const res = await axios.get("http://localhost:8080/api/project/all");
   dispatch({
     type: GET_PROJECTS,
     payload: res.data
