9fdc5fa57d332dc9f8818255a33833e6276fa65c
henok <henokkahsay43@gmail.com>
2019-10-31 13:33:57 -0700

    functionality

--- PPMToolFullStack/src/main/java/com/henok/ppmtool/services/ProjectTaskService.java
+++ PPMToolFullStack/src/main/java/com/henok/ppmtool/services/ProjectTaskService.java
@@ -1,5 +1,6 @@
 package com.henok.ppmtool.services;
 
+import com.henok.ppmtool.domain.Backlog;
 import com.henok.ppmtool.domain.ProjectTask;
 import com.henok.ppmtool.repositories.BacklogRepository;
 import com.henok.ppmtool.repositories.ProjectTaskRepository;
@@ -12,8 +13,32 @@ public class ProjectTaskService {
     private BacklogRepository backlogRepository;
     @Autowired
     private ProjectTaskRepository projectTaskRepository;
-    public ProjectTask addProjectTask()
-    {
-     
+
+    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
+
+        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+
+        projectTask.setBacklog(backlog);
+
+        Integer BackLongSequence = backlog.getPTSequence();
+
+        BackLongSequence++;
+
+        projectTask.setProjectSequence(projectIdentifier + "-" + BackLongSequence);
+
+        projectTask.setProjectIdentifier(projectIdentifier);
+
+     //   if (projectTask.getPriority() == 0 || projectTask.getPriority() == null) {
+            projectTask.setPriority(3);
+        //  }
+
+        if (projectTask.getStatus() == "" || projectTask.getStatus() == null) {
+                projectTask.setStatus("TO_DO");
+
+            }
+
+            return projectTaskRepository.save(projectTask);
+        }
     }
-}
+
+
