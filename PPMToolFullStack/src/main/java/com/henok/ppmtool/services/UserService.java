020695a11591811d17b739b143a2b49550e992dc
henok <henokkahsay43@gmail.com>
2019-12-05 13:56:18 -0800

    User Registration Part1

--- PPMToolFullStack/src/main/java/com/henok/ppmtool/services/UserService.java
+++ PPMToolFullStack/src/main/java/com/henok/ppmtool/services/UserService.java
@@ -1,8 +1,10 @@
 package com.henok.ppmtool.services;
 
 
+import com.henok.ppmtool.domain.User;
 import com.henok.ppmtool.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,5 +12,13 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+@Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public User saveUser (User newUser){
+newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+
+return  userRepository.save(newUser);
+    }
 
 }
