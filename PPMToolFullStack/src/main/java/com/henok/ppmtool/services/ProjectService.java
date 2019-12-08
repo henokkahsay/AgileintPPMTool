a9ea6490ca15c9874ee02cea51edf370f9cfe866
henok <henokkahsay43@gmail.com>
2019-12-08 07:27:38 -0800

    Lock operations to specific User<Update>

--- PPMToolFullStack/src/main/java/com/henok/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/com/henok/ppmtool/services/ProjectService.java
@@ -21,8 +21,19 @@ public class ProjectService {
 @Autowired
 private UserRepository userRepository;
     public Project saveOrUpdateProject(Project project,String username){
-        try{
 
+
+
+
+if (project.getId()!= null) {
+    Project existingProject = projectRepository.findByProjectIdentifier(project.getProjectIdentifier());
+    if (existingProject != null && (!existingProject.getProjectLeader().equals(username))) {
+    throw new ProjectNotFoundException("Project not Found in your account");
+}else if (existingProject  == null){
+        throw  new ProjectNotFoundException("Project With ID: '"+project.getProjectIdentifier()+"' cannot be updated because it doesn't exist");
+    }
+}
+try {
             User user= userRepository.findByUsername(username);
             project.setUser(user);
             project.setProjectLeader(user.getUsername());
