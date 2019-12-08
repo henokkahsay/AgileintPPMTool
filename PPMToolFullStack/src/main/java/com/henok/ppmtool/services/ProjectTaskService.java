a0d28d1b8f432562d81d31836a61096acd86ce61
henok <henokkahsay43@gmail.com>
2019-12-08 08:30:00 -0800

    Find,Update,Delete,Project task with Security

--- PPMToolFullStack/src/main/java/com/henok/ppmtool/services/ProjectTaskService.java
+++ PPMToolFullStack/src/main/java/com/henok/ppmtool/services/ProjectTaskService.java
@@ -60,11 +60,8 @@ public class ProjectTaskService {
 }
 
 
-public  ProjectTask findPtByProjectSequence(String backlog_id,String pt_id ){
-Backlog backlog=backlogRepository.findByProjectIdentifier(backlog_id);
-if (backlog==null){
-    throw new ProjectNotFoundException("Project with ID: '"+backlog_id+"' does not exist");
-}
+public  ProjectTask findPtByProjectSequence(String backlog_id,String pt_id ,String username){
+    projectService.findProjectByIdentifier(backlog_id,username);
 ProjectTask projectTask=projectTaskRepository.findByProjectSequence(pt_id);
 if (projectTask==null){
     throw new ProjectNotFoundException("Project Task '"+pt_id+"' not found");
@@ -78,14 +75,14 @@ if (!projectTask.getProjectIdentifier().equals(backlog_id)){
 return projectTask;
     }
 
-    public ProjectTask updateByProjectSequence(ProjectTask updatedTask, String backlog_id,String pt_id){
-        ProjectTask projectTask=findPtByProjectSequence(backlog_id,pt_id);
+    public ProjectTask updateByProjectSequence(ProjectTask updatedTask, String backlog_id,String pt_id,String username){
+        ProjectTask projectTask=findPtByProjectSequence(backlog_id,pt_id,username);
         projectTask=updatedTask;
         return projectTaskRepository.save(projectTask);
     }
-    public void deletePTByProjectSequence(String backlog_id,String pt_id){
-        ProjectTask projectTask=findPtByProjectSequence(backlog_id,pt_id);
-        projectTaskRepository.delete(projectTask);
+    public void deletePTByProjectSequence(String backlog_id,String pt_id ,String username){
+        ProjectTask projectTask= findPtByProjectSequence(backlog_id,pt_id,username);
+       projectTaskRepository.delete(projectTask);
 
     }
 
